08c91948161c503f2659d498c99c699156eac1ce
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2022-07-21 12:06:16 +0300

    added footer

---
+++ footer.js
@@ -0,0 +1,14 @@
+let footer = document.createElement("div")
+footer.innerHTML = `<footer>
+<div class="footer_content">
+    <h3>Stay Connected</h3>
+    <p> Follow Us on</p>
+    <ul class="socials">
+        <li><img src="./assets/facebook-brands.svg" height="50px" width="80px" alt="fb"</li>
+        <li><img src="./assets/instagram-square-brands.svg" height="50px" width="80px" alt="ig"</li>
+        <li><img src="./assets/twitter-square-brands.svg" height="50px" width="80px" alt="twt"</li>
+        <li><img src="./assets/youtube-square-brands.svg" height="50px" width="80px" alt="ytb"</li>
+    </ul>
+</div
+</footer>`
+document.body.prepend(footer)
